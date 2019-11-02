Added Time Convolution, fixed normalization, changed normalization to be over time

dataloader.py
@@ -18,7 +18,7 @@ PLOT_GRAD_FLOW = True
 NORMALIZE = True
 
 #writer = SummaryWriter(log_dir = './tb_logs/' + str(time.time()))
-writer = SummaryWriter(log_dir = './tb_logs/' + "embeddings")
+writer = SummaryWriter(log_dir = './tb_logs/' + 'cnn_run1')
 #####################################
 
 class MEG_Dataset(Data.Dataset):
@@ -42,7 +42,6 @@ class MEG_Dataset(Data.Dataset):
 print("Loading MEG data...")
 meg_dict = pickle_load(FILE_PATH)
 data = meg_dict['data_array']
-pdb.set_trace()
 stimulus_order_dict = meg_dict['stimulus_order_dict']
 inv_stimulus_order_dict = meg_dict['inv_stimulus_order_dict']
 question_order_dict = meg_dict['question_order_dict']
@@ -69,13 +68,15 @@ train_loader = Data.DataLoader(train_dataset,
                                batch_size = BATCH_SIZE, 
                                shuffle = True, 
                                drop_last = True)
+
+# add the dimension of 1 in index 1 because the dataset relies on 
+# having shape (words, questions, channels, time) so we need a dummy 
+# axis for the questions index
 test_data = np.mean(split_data[1], axis=1).reshape(split_data[1].shape[0],
                                                    1,
                                                    split_data[1].shape[2],
                                                    split_data[1].shape[3])
-print('TEST TYPE: ' + str(type(test_data)))
-print('TEST DIMS: ')
-print(test_data.shape)
+
 test_dataset = MEG_Dataset(test_data, test_map)
 test_loader = Data.DataLoader(test_dataset, 
                            batch_size = BATCH_SIZE, 

model.py
@@ -30,7 +30,7 @@ def calc_L_out(L_in, padding, kernel_size, stride, dilation=1):
 # figure out the feature dimension of the last layer to automate
 # entry of the in hidden size for the final linear classification layer 
 def get_last_L_out(params):
-    init_L_in = 500
+    init_L_in = MILLISECONDS
     L_out = None
     for i in range(len(params)):
         L_in = L_out if L_out != None else init_L_in
@@ -38,9 +38,9 @@ def get_last_L_out(params):
                                params[i][3])
     return L_out
 
-class Simple_Conv1d(nn.Module):
+class Space_Conv(nn.Module):
     def __init__(self, num_classes):
-        super(Simple_Conv1d, self).__init__()
+        super(Space_Conv, self).__init__()
         params = [[306, 16, 3, 2, 1],
                   [16,   4, 1, 2, 1] ]
 
@@ -61,6 +61,7 @@ class Simple_Conv1d(nn.Module):
         # calculate last length and multiply by the last out channels
         # because we are flattening it
         in_size = get_last_L_out(params) * params[-1][1]
+
         self.classification_model = nn.Sequential(
             nn.ReLU(True),
             nn.Linear(in_size, num_classes)
@@ -72,8 +73,50 @@ class Simple_Conv1d(nn.Module):
             return convolved
 
         else:
+
             convolved = self.embedding_model(x)
             return self.classification_model(convolved)
+
+class Time_Conv(nn.Module):
+    def __init__(self, num_classes):
+        super(Time_Conv, self).__init__()
+        def get_last_L_out_time(params):
+            init_L_in = 306
+            L_out = None
+            for i in range(len(params)):
+                L_in = L_out if L_out != None else init_L_in
+                L_out = calc_L_out(L_in, params[i][4], params[i][2], 
+                                       params[i][3])
+            return L_out
+
+        params = [[MILLISECONDS, 750, 3, 2, 1]]
+
+        self.embedding_model = nn.Sequential(
+            nn.Conv1d(in_channels = params[0][0], 
+                out_channels = params[0][1], 
+                kernel_size = params[0][2],
+                stride=params[0][3], padding=params[0][4], bias=True),   
+            Flatten()     
+            )
+
+        # calculate last length and multiply by the last out channels
+        # because we are flattening it
+        in_size = get_last_L_out_time(params) * params[-1][1]
+
+        self.classification_model = nn.Sequential(
+            nn.ReLU(), 
+            nn.Linear(in_size, num_classes)
+            )    
+
+    def forward(self, x, embedding=False):
+        if embedding:
+            convolved = self.embedding_model(x)
+            return convolved
+
+        else:
+            convolved = self.embedding_model(x)
+            return self.classification_model(convolved)
+
 
 def weights_init_uniform(m):
     classname = m.__class__.__name__

train.py
@@ -112,10 +112,11 @@ class Trainer(object):
             if NORMALIZE: 
                 # normalizing each channel by subtracting channel mean 
                 # and dividing by channel st dev 
-                means = torch.mean(data, dim = 2).view(32, 306, 1)
-                sds = torch.mean(data, dim = 2).view(32, 306, 1)
+                means = torch.mean(data, dim = 1).view(32, 1, 750)
+                sds = torch.std(data, dim = 1).view(32, 1, 750)
                 data = (data - means) / sds
 
+            data = data.permute(0, 2, 1)
             out = self.model(data, batch_idx)
 
             out = out.view(self.batch_size, -1)
@@ -171,31 +172,34 @@ class Trainer(object):
 
                 self.optimizer.zero_grad()
 
-                writer.add_embedding(mat = data.view(BATCH_SIZE, -1), 
-                                     global_step = batch_idx, 
-                                     metadata = [word for word in label_word], 
-                                     tag = 'Unormalized MEG Vecs')
+                #writer.add_embedding(mat = data.view(BATCH_SIZE, -1), 
+                #                     global_step = batch_idx, 
+                #                     metadata = [word for word in label_word], 
+                #                     tag = 'Unormalized MEG Vecs')
 
                 if NORMALIZE: 
                     # normalizing each channel by subtracting channel mean 
                     # and dividing by channel st dev 
-                    means = torch.mean(data, dim = 2).view(32, 306, 1)
-                    sds = torch.mean(data, dim = 2).view(32, 306, 1)
+                    means = torch.mean(data, dim = 1).view(32, 1, 750)
+                    sds = torch.std(data, dim = 1).view(32, 1, 750)
                     data = (data - means) / sds
 
-                writer.add_embedding(mat = data.view(BATCH_SIZE, -1), 
-                                     global_step = batch_idx, 
-                                     metadata = [word for word in label_word], 
-                                     tag = 'Normalized MEG Vecs')
+                #writer.add_embedding(mat = data.view(BATCH_SIZE, -1), 
+                #                     global_step = batch_idx, 
+                #                     metadata = [word for word in label_word], 
+                #                     tag = 'Normalized MEG Vecs')
 
+                # FOR TIME CONVOLUTION 
+                # want to have the time steps as the channels
+                data = data.permute(0, 2, 1)
                 out = self.model(data, batch_idx)
 
                 out = out.view(self.batch_size, -1)
                 loss = self.criterion(out, label)
                 loss.backward()
                 
-                plot_grad_flow(self.model.named_parameters())
-                plot_grad_flow_alt(self.model.named_parameters())
+                #plot_grad_flow(self.model.named_parameters())
+                #plot_grad_flow_alt(self.model.named_parameters())
 
                 self.optimizer.step()
 
@@ -251,7 +255,8 @@ def main():
     print("Creating model and optimizer...")
     NUM_WORDS = 60
     #model = Logistic_Regression(NUM_WORDS)
-    model = torch.load("saved_models/epoch9.pt")
+    model = Time_Conv(NUM_WORDS)
+    #model = torch.load("saved_models/epoch9.pt")
     optim = torch.optim.Adam(model.parameters(), 
                              lr = 1e-3)
 